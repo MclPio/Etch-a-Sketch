dcb80e04ba419984fc8b73dea5af6f1f4c6e4740
MclPio <113801014+MclPio@users.noreply.github.com>
2022-12-22 14:43:43 -0500

    add grid generation style

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const container = document.querySelector("#container");
-let square = [];
+let square = []; //array to hold grid
+let grid; //for storing grid size
 function createGrid(size = 16) {
     for (let i = 0; i < size**2; i++) {
         square[i] = document.createElement('div');
         square[i].className = "square";
         container.appendChild(square[i]);
+        grid = size;
     }
 }
+
+createGrid();
