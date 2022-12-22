ac4ea292c1a033956d66e2286046ff89740c1772
MclPio <113801014+MclPio@users.noreply.github.com>
2022-12-22 18:52:27 -0500

    add Shade button function

--- script.js
+++ script.js
@@ -18,9 +18,9 @@ function createGrid(size = 16) {
     })
 }
 
-function colorGrid (color = 'colorBlack') {
+function colorGrid (color = 'black') {
     square.forEach((item) => item.addEventListener('mouseover', () => {
-        item.classList.add(color); 
+        item.style.backgroundColor = color; 
     }))  
 }
 
@@ -40,6 +40,42 @@ function buttonClick () {
     })
 }
 
+const blackButton = document.querySelector('#colorBlack');
+const rgbButton = document.querySelector('#colorRGB');
+const shadeButton = document.querySelector('#colorShade');
+const rgbColors = ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#00FFFF', 
+'#FF00FF','#800000', '#808000', '#008000', '#800080', '#008080', '#000080'];
+rgbButton.addEventListener('click', () => {
+    square.forEach((item) => item.remove());
+    createGrid(grid);
+    square.forEach((item) => item.addEventListener('mouseover', () => {
+        item.style.backgroundColor = rgbColors[Math.floor(Math.random()*rgbColors.length)];
+    }))  
+})
+
+blackButton.addEventListener('click', () => {
+    colorGrid(); 
+})
+
+shadeButton.addEventListener('click', () => {
+    square.forEach((item) => item.remove());
+    createGrid(grid);
+    square.forEach((item) => item.addEventListener('mouseover', () => {
+        item.style.backgroundColor = 'rgb(255,255,255)';
+        item.addEventListener('mouseover', () => {
+            let i = item.style.backgroundColor; // returns string rgb value example: rgb(255, 0, 255)
+            i = i.slice(4,-1).split(",")
+            let red = Number(i[0]);
+            let green = Number(i[1]);
+            let blue = Number(i[2]);
+            red *= 0.9;
+            green *= 0.9;
+            blue *= 0.9;
+            i = item.style.backgroundColor = `rgb(${red},${green},${blue})`;
+        })
+
+    })) 
+})
 createGrid();
 buttonClick();
 colorGrid();
